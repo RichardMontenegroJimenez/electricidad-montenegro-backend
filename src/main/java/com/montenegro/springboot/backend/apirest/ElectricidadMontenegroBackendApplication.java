a8c92a7af97abe7f41f99fd3f5c0758bc8cb28ee
Richard <1903023@alu.murciaeduca.es>
2022-05-25 19:01:30 +0200

    initial commit

---
+++ src/main/java/com/montenegro/springboot/backend/apirest/ElectricidadMontenegroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.montenegro.springboot.backend.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricidadMontenegroBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricidadMontenegroBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/montenegro/springboot/backend/apirest/ElectricidadMontenegroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.montenegro.springboot.backend.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectricidadMontenegroBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
